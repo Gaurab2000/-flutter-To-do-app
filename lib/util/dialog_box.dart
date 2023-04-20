3b708bc42638ad5089798ed0bcea54a6ca940162
Gaurab2000 <gaurabrayamajhi.075@kathford.edu.np>
2023-04-20 17:18:27 +0545

    initial commit

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class DialogBox extends StatelessWidget {
+  const DialogBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog( backgroundColor: Colors.yellow[300],
+      content: Container(
+        height: 120,
+        child: Column(
+       
+          children: [
+            // get user input
+            TextField(
+              
+              ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
